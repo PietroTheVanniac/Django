3cea0519f4895494cfa13deb9aec9ee7d60c0906
Jesus Pietrucci <pietroj0211@gmail,com>
2024-01-27 11:26:37 -0400

    delete

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import TemplateView,ListView, DetailView, CreateView
+from django.views.generic import TemplateView,ListView, DetailView, CreateView, DeleteView
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model=Post
@@ -22,3 +23,8 @@ class BlogUpdateView(UpdateView):
      model=Post
      template_name="post_edit.html"
      fields=["title", "body"]
+
+class BlogDeleteView(DeleteView):
+     model=Post
+     template_name="post_delete.html"
+     success_url= reverse_lazy("home")
